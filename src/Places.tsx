19ddd887395538711e2fbfadebb4f6a2f73736b8
fraukesch <frauke.schmidt@bettermarks.com>
2024-08-25 12:22:37 +0200

    fix: prettier styling and open street maps instead of google maps

--- src/Places.tsx
+++ src/Places.tsx
@@ -1,21 +1,61 @@
-import {AspectRatio, Box, Heading, VStack, Wrap, WrapItem, Text, Button} from "@chakra-ui/react";
+import {AspectRatio, Box, Heading, VStack, Wrap, WrapItem, Text, Button, Link} from "@chakra-ui/react";
 import {useLocation} from "react-router-dom";
 
 export const Places = () => (
-    <VStack>
-        <Wrap>
-            <WrapItem>
-                <AspectRatio width={300} ratio={16 / 9}>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4845.8046220289625!2d13.383870891694544!3d52.607536604500204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a852c5ed869645%3A0x454fff67655b0f2f!2sSchillingweg%2C%2013159%20Berlin!5e0!3m2!1sde!2sde!4v1724522136909!5m2!1sde!2sde"
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"></iframe>
-                </AspectRatio>
-            </WrapItem>
-            <WrapItem>
+    <VStack alignItems='flex-start'>
+        <Wrap p={4} m={4} shadow='md' borderWidth='1px' width='calc(100% - 2rem)'>
+            <WrapItem width={{
+                base: 'calc(100% - 2rem)',
+                sm: 'calc(100% - 2rem)',
+                md: 'calc(50% - 1rem)'
+            }}>
                 <Box>
                     <Heading>Rosenthal</Heading>
                     <Text>Buddeln am Herrenberg</Text>
+                </Box>
+            </WrapItem>
+            <WrapItem width={{
+                base: 'calc(100% - 2rem)',
+                sm: 'calc(100% - 2rem)',
+                md: 'calc(50% - 1rem)'
+            }}  flexDir='column'>
+                <AspectRatio width='100%' ratio={16 / 9}>
+                    <iframe
+                        src="https://www.openstreetmap.org/export/embed.html?bbox=13.363623619079592%2C52.59593150291951%2C13.417439460754396%2C52.622721380662426&amp;layer=mapnik&amp;marker=52.609328490428375%2C13.390531539916992"
+                    ></iframe>
+                </AspectRatio>
+                <Text>
+                    <Link href='https://www.openstreetmap.org/?mlat=52.60933&amp;mlon=13.39053#map=15/52.60933/13.39053'>
+                        Größere Karte anzeigen
+                    </Link>
+                </Text>
+            </WrapItem>
+        </Wrap>
+        <Wrap p={4} m={4} shadow='md' borderWidth='1px' width='calc(100% - 2rem)'>
+            <WrapItem flexDir='column'width={{
+                base: 'calc(100% - 2rem)',
+                sm: 'calc(100% - 2rem)',
+                md: 'calc(50% - 1rem)'
+            }}>
+                <AspectRatio width='100%' ratio={16 / 9}>
+                    <iframe
+                        src="https://www.openstreetmap.org/export/embed.html?bbox=13.336157798767092%2C52.61099631096721%2C13.389973640441895%2C52.63777697448701&amp;layer=mapnik&amp;marker=52.62438869107126%2C13.363065719604492"
+                    ></iframe>
+                </AspectRatio>
+                <Text>
+                    <Link
+                        href="https://www.openstreetmap.org/?mlat=52.62439&amp;mlon=13.36307#map=15/52.62439/13.36307">Größere
+                        Karte anzeigen</Link>
+                </Text>
+            </WrapItem>
+            <WrapItem width={{
+                base: 'calc(100% - 2rem)',
+                sm: 'calc(100% - 2rem)',
+                md: 'calc(50% - 1rem)'
+            }}>
+                <Box>
+                    <Heading>Tegeler Fließ</Heading>
+                    <Text>Erfrischen</Text>
                 </Box>
             </WrapItem>
         </Wrap>

f6de7f5af744d66bcfd1c4ef90a5e124f2d44d65
fraukesch <frauke.schmidt@bettermarks.com>
2025-01-11 19:52:45 +0100

    feat: add Miro

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -5,9 +5,18 @@ import gruno from '../assets/freunde/gruno/gruno_elbe.webp';
 import buddelnMitGruno1 from '../assets/freunde/gruno/buddeln_mit_gruno_1.webp';
 import buddelnMitGruno2 from '../assets/freunde/gruno/buddeln_mit_gruno_2.webp';
 
+import miro1 from '../assets/freunde/miro/miro1.webp';
+import miro2 from '../assets/freunde/miro/miro2.webp';
+import miro3 from '../assets/freunde/miro/miro3.webp';
+
 
 export const Friends = () => {
     return <VStack alignItems='flex-start'>
+        <PictureCollectionWithText
+            headline='Miro'
+            text='Der Hund, der Punta zu uns gebracht hat. Danke Miro, Danke Katha, und vor allem Danke Lene!'
+            pictures={[miro1, miro2, miro3]}
+        />
         <PictureCollectionWithText
             headline='Gruno'
             text='Ein Riese mit riesen Herz. Nie vergessen für die Gabe seiner Knochendecke. Puntas erster Buddel-Buddy.'

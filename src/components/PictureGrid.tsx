0ce6085b1ba5aa33947f95dae816a705173cedfa
fraukesch <frauke.schmidt@bettermarks.com>
2025-06-15 20:49:48 +0200

    fix: min width for mobile

--- src/components/PictureGrid.tsx
+++ src/components/PictureGrid.tsx
@@ -2,7 +2,7 @@ import {SimpleGrid} from '@chakra-ui/react';
 import {PropsWithChildren} from 'react';
 
 export const PictureGrid = ({children}: PropsWithChildren) => (
-    <SimpleGrid minChildWidth='400px' gap={6}>
+    <SimpleGrid minChildWidth='350px' gap={6}>
         {children}
     </SimpleGrid>
 )
